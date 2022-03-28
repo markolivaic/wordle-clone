5e8e76739d7a68baa25ea895476e952b644df346
Marko Livaic <makifrane@gmail.com>
2022-03-28 21:04:53 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import "./App.css";
 import Board from "./Components/Board";
 import Keyboard from "./Components/Keyboard";
+import { boardDefault } from "./Words";
+import { useState, createContext } from "react";
+
+export const AppContext = createContext();
 
 function App() {
+  const [board, setBoard] = useState(boardDefault);
   return (
     <div className="App">
       <nav>
         <h1>Wordle</h1>
       </nav>
-      <Board />
-      <Keyboard />
+      <AppContext.Provider value={{ board, setBoard }}>
+        <Board />
+        <Keyboard />
+      </AppContext.Provider>
     </div>
   );
 }
